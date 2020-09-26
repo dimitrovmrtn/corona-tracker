fdd9577dda25bd9752be6c2ad461247209236594
Martin Dimitrov <dimitrovmrtn@gmail.com>
2020-09-26 15:14:19 +0300

    Updates

---
+++ precache-manifest.6c9a168b74e7bba2e8427518f8a3ead7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3197b70517217c6cb9f4c1ea993bf4d5",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "99ea0638ca84f68842f5",
+    "url": "/corona-tracker/static/css/main.f99c135c.chunk.css"
+  },
+  {
+    "revision": "5b7b2f6a84caf98b1353",
+    "url": "/corona-tracker/static/js/2.ce0e7bd4.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/corona-tracker/static/js/2.ce0e7bd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ea0638ca84f68842f5",
+    "url": "/corona-tracker/static/js/main.18ff21dc.chunk.js"
+  },
+  {
+    "revision": "1466a8df3292ab708200",
+    "url": "/corona-tracker/static/js/runtime-main.3cef4ccd.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
